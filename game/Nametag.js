43eb1b4925b28c67bcbb0a38e4468186e22208b6
TomKrcmar <tomkrcmar@gmail.com>
2015-09-12 18:44:40 -0400

    Removed old code

--- game/Nametag.js
+++ game/Nametag.js
@@ -1,40 +1,11 @@
-var Nametag = function(name, userData) {
+var Nametag = function() {
 	OE.GameObject.call(this);
 };
 Nametag.prototype = {
-	
 	onCreate: function() {
-		Agent.prototype.onCreate.call(this);
-		
-		this.target = new OE.Vector2();
-		this.findNewTarget();
+		//this.addChild(new OE.DrawableText(this.name));
 	},
-	
 	onUpdate: function() {
-		Agent.prototype.onUpdate.call(this);
-		var pos = this.getPos();
-		var dx = this.target.x - pos.x;
-		var dy = this.target.y - pos.z;
-		var d2 = dx*dx + dy*dy;
-		
-		if (d2 < 10.0) {
-			if (Math.random() < 0.0015) {
-				this.findNewTarget();
-			}
-		}
-		else {
-			var d = Math.sqrt(d2);
-			var nx = dx/d;
-			var ny = dy/d;
-			pos.x += nx * this.speed;
-			pos.z += ny * this.speed;
-			this.setPos(pos);
-		}
-	},
-	
-	findNewTarget: function() {
-		this.target.setf(Math.random() * 200.0 - 100.0,
-						 Math.random() * 200.0 - 100.0);
 	}
 };
 OE.Utils.defClass(Nametag, OE.GameObject);
